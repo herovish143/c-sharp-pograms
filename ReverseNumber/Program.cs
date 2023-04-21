515a31d7dd4719d13061c26cc76320170571bd29
Vishal Pal <vishal@zonvoir.com>
2023-04-21 17:55:18 +0530

    adding the reverse number

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/**
+ *  WAP to Reverse a Number
+ */
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, reverse = 0;
+
+            Console.WriteLine("Please enter the number.");
+            num = int.Parse(Console.ReadLine());
+
+            while (num != 0)
+            {
+                reverse *= 10;
+                reverse += num % 10;
+                num /= 10;
+            }
+
+            Console.WriteLine("Reversed number is: {0} ", reverse);
+            Console.ReadKey();
+        }
+    }
+}
